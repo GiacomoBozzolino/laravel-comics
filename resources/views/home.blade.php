66565a4d2a1edc3fe2f57e8e1fed726a406b26ec
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-07-21 17:03:12 +0200

    [FEAT] inizio lavoro main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,6 +13,7 @@
     <!-- Styles -->
     @vite('resources/js/app.js')
 
+
 </head>
 
     
@@ -20,8 +21,32 @@
 
     @include('partials.header')
 
-
     <main >
+        <div >
+            <img id="jumbo"  class="flex" src="{{ Vite::asset('resources/images/jumbotron.jpg')}}" >
+        </div>
+        <div class="black">
+            <div class="container content">
+                <div id="series">CURRENT SERIES</div>
+                <div class="flex flex-wrap">
+                    <AppComics v-for="(comics, index) in NewComics" :key="index" :recentComics='comics'  /> 
+                </div>
+                
+            </div>
+        </div>
+        <div class="blue">
+            <div class="container blue-content flex">
+                <ul class="flex">
+                    @foreach($products as $product)
+                    <li >
+                        <a href="#">
+                            <img :src="{{}}" alt="">
+                            <div></div>
+                        </a>
+                    </li>   
+                </ul>
+            </div>
+        </div>
         
     </main>
 
