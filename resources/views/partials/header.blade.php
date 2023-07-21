93cd6d316ac5b21982c987946939c4c93a2abe84
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-07-21 16:42:20 +0200

    [FEAT] creazione partials header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,56 @@
+<header class="container d-flex">
+        <div class="row">
+            <div class="col-12">
+                <div class="d-flex justify-content-end align-items-center bg-primary">
+                    <div class="px-2 px-1">
+                        DC POWER VISA
+                    </div>
+                    <div class="px-2 px-1 ">
+                        ADDITIONAL DC SITE
+                    </div>
+                </div>
+            </div>
+            <div class=" col-12 d-flex flex-wrap align-items-center justify-content-between py-3"> 
+                <div >
+                    <img src="{{ Vite::asset('resources/images/dc-logo.png')}}" alt="">
+                </div>
+                <div>
+                    <ul class="d-flex flex-wrap">
+                        <li >
+                            <a href="">CHARACTERS</a>
+                        </li>
+                        <li >
+                             <a href="{{ route ('homepage') }}">COMICS</a>
+                        </li>
+                        <li >
+                            <a href="">MOVIES</a>
+                        </li>
+                        <li >
+                            <a href="">TV</a>
+                        </li>
+                        <li >
+                            <a href="">GAMES</a>
+                        </li>
+                        <li >
+                             <a href="">COLLECTIBLES</a>
+                        </li>
+                        <li >
+                            <a href="">VIDEOS</a>
+                        </li>
+                        <li >
+                            <a href="">FANS</a>
+                        </li>
+                        <li >
+                            <a href="">NEWS</a>
+                        </li>
+                        <li >
+                            <a href="">SHOP</a>
+                        </li>
+                        <li >
+                            <input type="search" name="search" id="search" placeholder="search">
+                        </li>
+                    </ul>
+                </div>
+            </div>
+       </div>
+    </header>
