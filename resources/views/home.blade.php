b263e04180a12c61b7d7e78ecfe4590db1a75636
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-07-21 16:04:50 +0200

    [FEAT] inizio lavoro header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,10 +15,52 @@
 
 </head>
 
+    
 <body>
-    <header>
-        
+
+
+
+    <header class="container d-flex">
+        <div>
+            <img src="" alt="">
+        </div>
+        <div>
+            <ul class="d-flex">
+                <li >
+                   <a href="">CHARACTERS</a>
+                </li>
+                <li >
+                   <a href="{{ route ('homepage') }}">COMICS</a>
+                </li>
+                <li >
+                   <a href="">MOVIES</a>
+                </li>
+                <li >
+                   <a href="">TV</a>
+                </li>
+                <li >
+                   <a href="">GAMES</a>
+                </li>
+                <li >
+                   <a href="">COLLECTIBLES</a>
+                </li>
+                <li >
+                   <a href="">VIDEOS</a>
+                </li>
+                <li >
+                   <a href="">FANS</a>
+                </li>
+                <li >
+                   <a href="">NEWS</a>
+                </li>
+                <li >
+                   <a href="">SHOP</a>
+                </li>
+
+            </ul>
+        </div>
     </header>
+
 
     <main >
         
